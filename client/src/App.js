b2f29c4bc1082dd9339f855c487ca4907858cad0
Naisha <naishs2@cs.washington.edu>
2024-06-23 19:37:58 -0700

    redo react app creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-// Create and manage routes for application
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import TeamPerformanceAnalysis from './pages/TeamPerformanceAnalysis';
-import MatchupAnalyzer from './pages/MatchupAnalyzer';
-import FantasyRecommendations from './pages/FantasyRecommendations';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/team-performance-analysis" element={<TeamPerformanceAnalysis />} />
-        <Route path="/matchup-analyzer" element={<MatchupAnalyzer />} />
-        <Route path="/fantasy-recommendations" element={<FantasyRecommendations />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
